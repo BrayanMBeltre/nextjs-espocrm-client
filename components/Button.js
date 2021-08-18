39f805c4830c1040e2a6eb2cfa1b7b82eaa82585
BrayanMBeltre <brayanmb07@gmail.com>
2021-08-17 20:52:43 -0400

    button color fixed

--- components/Button.js
+++ components/Button.js
@@ -1,4 +1,5 @@
-export function Button({ className, color = "indigo", children, ...props }) {
+export function Button({ className, color, children, ...props }) {
+  color ? color : "indigo";
   props.className = `${className} flex items-center justify-center px-4 py-2 border-2 border-${color}-600 rounded-md font-semibold text-${color}-600 hover:bg-${color}-700 hover:text-${color}-50 transition duration-300 ease-in-out`;
 
   return <button {...props}>{children}</button>;
